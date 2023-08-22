fdf4bdddb120356473e92d3ff93ab8dc023617c8
Unto Pulkkinen <unto@Untos-MacBook-Pro.local>
2023-08-22 15:25:12 +0300

    started app, added FlatList, todo: flex

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Button, Text, View, FlatList, TouchableOpacity, Image, SafeAreaView} from 'react-native';
 
-export default function App() {
+const mediaArray = [
+  {
+    'key': '0',
+    'title': 'Title 1',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/161',
+    },
+    'filename': 'http://placekitten.com/2048/1920',
+  },
+  {
+    'key': '1',
+    'title': 'Title 2',
+    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/164',
+    },
+    'filename': 'http://placekitten.com/2041/1922',
+  },
+  {
+    'key': '2',
+    'title': 'Title 3',
+    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/167',
+    },
+    'filename': 'http://placekitten.com/2039/1920',
+  },
+];
+
+export default App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={mediaArray}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity>
+              <Image
+                style={styles.image}
+                source={{uri: item.thumbnails.w160}}
+              />
+              <View style={styles.text}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text>{item.description}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexWrap: 'wrap'
   },
+  image: {
+    width: 200,
+    height: 400,
+    backgroundColor: 'lightgrey'
+  },
+  text: {
+    backgroundColor: 'lightgrey', padding: 20
+  },
+  title: {
+    fontSize: 20,
+    fontWeight:'bold'
+  }
 });
