c804837c5e13c0f2f328a642ea66402fb43dac3a
Unto Pulkkinen <unto@Untos-MacBook-Pro.local>
2023-08-25 10:27:31 +0300

    added components and props

--- App.js
+++ App.js
@@ -1,87 +1,20 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Button, Text, View, FlatList, TouchableOpacity, Image, SafeAreaView} from 'react-native';
-
-const mediaArray = [
-  {
-    'key': '0',
-    'title': 'Title 1',
-    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/161',
-    },
-    'filename': 'http://placekitten.com/2048/1920',
-  },
-  {
-    'key': '1',
-    'title': 'Title 2',
-    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/164',
-    },
-    'filename': 'http://placekitten.com/2041/1922',
-  },
-  {
-    'key': '2',
-    'title': 'Title 3',
-    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/167',
-    },
-    'filename': 'http://placekitten.com/2039/1920',
-  },
-];
-
-export default App = () => {
+import {StyleSheet, SafeAreaView} from 'react-native';
+import List from './components/List';
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={mediaArray}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity>
-             
-              <View style={styles.textAndImage}>
-              <Image
-
-                style={styles.image}
-                source={{uri: item.thumbnails.w160}}
-              />
-              <View style={styles.text}>
-                <Text style={styles.title}>{item.title}</Text>
-                <Text>{item.description}</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
-      />
+    <>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
   },
-  image: {
-    width: '50%',
-    height: '100%',
-    backgroundColor: 'lightgrey',
-    alignSelf: 'flex-start',
-  },
-  textAndImage: {
-    flex: 1,
-    backgroundColor: 'lightgrey', padding: 20,
-    marginBottom: 20,
-    flexDirection: 'row',
-  },
-  text: {
-    padding: 20,
-    width: 200,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight:'bold'
-  }
-});
+})
+
+export default App;
---
+++ components/ListItem.js
@@ -0,0 +1,48 @@
+import {Image, Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+const ListItem = (props) => {
+  return (
+    <TouchableOpacity>
+
+      <View style={styles.textAndImage}>
+        <Image
+          style={styles.image}
+          source={{uri: props.singleMedia.thumbnails.w160}}
+        />
+        <View style={styles.text}>
+          <Text style={styles.title}>{props.singleMedia.title}</Text>
+          <Text>{props.singleMedia.description}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: '50%',
+    height: '100%',
+    backgroundColor: 'lightgrey',
+    alignSelf: 'flex-start',
+  },
+  textAndImage: {
+    flex: 1,
+    backgroundColor: 'lightgrey', padding: 20,
+    marginBottom: 20,
+    flexDirection: 'row',
+  },
+  text: {
+    padding: 20,
+    width: 200,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
+export default ListItem;
