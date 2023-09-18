f6095145eadc5424abae47ebd65f9a2599113e32
Unto Pulkkinen <untop@k-d557-mac-15.pk.amk.root>
2023-09-18 15:52:46 +0300

    fullscreen video when landscape

--- views/Single.js
+++ views/Single.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import {mediaUrl} from '../utils/app-config';
 import {formatDate} from '../utils/functions';
@@ -7,11 +7,19 @@ import {Video} from 'expo-av';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useFavourite, useUser} from '../hooks/ApiHooks';
 import {Button} from '@rneui/base';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import {ScrollView} from 'react-native';
+import {MainContext} from '../contexts/MainContext';
 
 const Single = ({route, navigation}) => {
   const [owner, setOwner] = useState({});
+  const [userLike, setUserLike] = useState(false);
+  const [likes, setLikes] = useState([]);
+  const {user} = useContext(MainContext);
   const {getUserById} = useUser();
   const {postFavourite, getFavouritesById, deleteFavourite} = useFavourite();
+
+  const videoRef = useRef(null);
 
   const {
     title,
@@ -39,54 +47,131 @@ const Single = ({route, navigation}) => {
     try {
       const token = await AsyncStorage.getItem('userToken');
       const response = await postFavourite({file_id: fileId}, token);
-      console.log('createFavourite', response);
+      response && setUserLike(true);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+ const removeFavourite = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const response = await deleteFavourite(fileId, token);
+      response && setUserLike(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const fetchLikes = async () => {
+    try {
+      const likesData = await getFavouritesById(fileId);
+      setLikes(likesData);
+      likesData.forEach((like) => {
+        if (like.user_id === user.user_id) {
+          setUserLike(true);
+        }
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  // fullscreen vide on landscape
+  const unlockOrientation = async () => {
+    try {
+      await ScreenOrientation.unlockAsync();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  const lockOrientation = async () => {
+    try {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT_UP,
+      );
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const showvideoInFullscreen = async () => {
+    try {
+      await videoRef.current.presentFullscreenPlayer();
     } catch (error) {
       console.error(error.message);
     }
   };
 
   useEffect(() => {
+    fetchLikes();
+  }, [userLike]);
+
+  useEffect(() => {
+    unlockOrientation();
     fetchOwner();
+
+    // fullscreen video on landscape
+    const orientSub = ScreenOrientation.addOrientationChangeListener(
+      (event) => {
+        if (event.orientationInfo.orientation > 2) {
+          videoRef.current && showvideoInFullscreen();
+        }
+      },
+    );
+
+    return () => {
+      ScreenOrientation.removeOrientationChangeListener(orientSub);
+      lockOrientation();
+    };
   }, []);
 
   // Show full image and metadata
   return (
-    <Card>
-      <Card.Title>{title}</Card.Title>
-      {mediaType === 'image' ? (
-        <Card.Image
-          source={{uri: mediaUrl + filename}}
-          resizeMode="center"
-          style={{height: 300}}
-        />
-      ) : (
-        <Video
-          source={{uri: mediaUrl + filename}}
-          style={{height: 300}}
-          useNativeControls={true}
-          shouldPlay={true}
-          isLooping={true}
-        />
-      )}
-      <ListItem>
-        <Text>{description}</Text>
-      </ListItem>
-      <ListItem>
-        <Icon name="save" />
-        <Text>{Math.round(filesize / 1024)} kB</Text>
-      </ListItem>
-      <ListItem>
-        <Icon name="today" />
-        <Text>{formatDate(timeAdded)}</Text>
-      </ListItem>
-      <ListItem>
-        <Icon name="person" />
-        <Text>username: {owner.username}</Text>
-      </ListItem>
-      <ListItem>
-        <Button onPress={createFavourite} title={'Like'} />
-      </ListItem>
-    </Card>
+    <ScrollView>
+      <Card>
+        <Card.Title>{title}</Card.Title>
+        {mediaType === 'image' ? (
+          <Card.Image
+            source={{uri: mediaUrl + filename}}
+            resizeMode="center"
+            style={{height: 300}}
+          />
+        ) : (
+          <Video
+            source={{uri: mediaUrl + filename}}
+            style={{height: 300}}
+            useNativeControls={true}
+            shouldPlay={true}
+            isLooping={true}
+            ref={videoRef}
+          />
+        )}
+        <ListItem>
+          <Text>{description}</Text>
+        </ListItem>
+        <ListItem>
+          <Icon name="save" />
+          <Text>{Math.round(filesize / 1024)} kB</Text>
+        </ListItem>
+        <ListItem>
+          <Icon name="today" />
+          <Text>{formatDate(timeAdded)}</Text>
+        </ListItem>
+        <ListItem>
+          <Icon name="person" />
+          <Text>username: {owner.username}</Text>
+        </ListItem>
+        <ListItem>
+          {userLike ? (
+            <Button onPress={removeFavourite} title={'Unlike'} />
+          ) : (
+            <Button onPress={createFavourite} title={'Like'} />
+          )}
+          <Text>Total likes: {likes.length}</Text>
+        </ListItem>
+      </Card>
+    </ScrollView>
   );
 };
 
